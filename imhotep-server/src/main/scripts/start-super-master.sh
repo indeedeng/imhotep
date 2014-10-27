7d64a4caaedf4687e5cc246641c81dd39f1dbceb
darren <darren@indeed.com>
2014-10-27 14:34:44 -0500

    Remove unnecessary files.

--- imhotep-server/src/main/scripts/start-super-master.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source `dirname $BASH_SOURCE`/imhotep-env.sh
-
-mkdir -p $SQUALL_SHARDMANAGER_LOG_DIR
-
-$JAVA_HOME/bin/java -cp $HADOOP_HOME/conf:`readlink -f $SQUALL_SHARDMANAGER_LIB_DIR`/\* \
-    com.indeed.squall.shardmanager.supermaster.SuperMasterMain --springfile $SUPER_MASTER_CONFIG \
-    >& $SQUALL_SHARDMANAGER_LOG_DIR/supermaster-${SUPER_MASTER_CONFIG}.log &
--- imhotep-server/src/main/scripts/stop-daemon.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source `dirname $0`/imhotep-env.sh
-
-$JAVA_HOME/bin/java -cp `readlink -f $IMHOTEP_LIB_DIR`/\* com.indeed.imhotep.service.ImhotepDaemon asdf --port $IMHOTEP_DAEMON_PORT --shutdown
