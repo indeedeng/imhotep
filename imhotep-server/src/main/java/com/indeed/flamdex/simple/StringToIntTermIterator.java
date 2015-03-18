d9359a333e3f05f0bd5a0a3f2e7804520b541a69
Vladimir Markin <vladimir@indeed.com>
2015-03-17 21:35:41 -0500

    IMTEPD-155: When a Flamdex String field is pushed as a metric try to convert the terms to Ints

---
+++ imhotep-server/src/main/java/com/indeed/flamdex/simple/StringToIntTermIterator.java
@@ -0,0 +1,52 @@
+package com.indeed.flamdex.simple;
+
+/**
+ * @author vladimir
+ */
+
+public class StringToIntTermIterator implements SimpleIntTermIterator  {
+    private final SimpleStringTermIterator stringTermIterator;
+
+    public StringToIntTermIterator(SimpleStringTermIterator stringTermIterator) {
+        this.stringTermIterator = stringTermIterator;
+    }
+
+    @Override
+    public void reset(long term) {
+        stringTermIterator.reset(String.valueOf(term));
+    }
+
+    @Override
+    public long term() {
+        try {
+            return Long.valueOf(stringTermIterator.term());
+        } catch(NumberFormatException ignored) {
+            return 0;
+        }
+    }
+
+    @Override
+    public String getFilename() {
+        return stringTermIterator.getFilename();
+    }
+
+    @Override
+    public long getOffset() {
+        return stringTermIterator.getOffset();
+    }
+
+    @Override
+    public boolean next() {
+        return stringTermIterator.next();
+    }
+
+    @Override
+    public int docFreq() {
+        return stringTermIterator.docFreq();
+    }
+
+    @Override
+    public void close() {
+        stringTermIterator.close();
+    }
+}
