dc7e620869312789f5c7468eb4b50ada6b905acd
John C. Finley <johnf@indeed.com>
2015-05-19 12:36:26 -0500

    Added Splitter ctor with explicit term_iterator.

--- imhotep-server/src/main/native/optimized_ftgs/optimized/include/splitter.hpp
+++ imhotep-server/src/main/native/optimized_ftgs/optimized/include/splitter.hpp
@@ -15,49 +15,67 @@
 
 namespace imhotep {
 
-    /** !@# todo(johnf): Splitter either needs to produce doc
-         addresses instead of offsets or somehow preserve doc base
-         address (or file). */
+    template <typename term_t> struct SplitterTraits { typedef void                 iterator_t; };
+    template <> struct SplitterTraits<IntTerm>       { typedef int_term_iterator    iterator_t; };
+    template <> struct SplitterTraits<StringTerm>    { typedef string_term_iterator iterator_t; };
+
     template <typename term_t>
     class Splitter {
     public:
+        typedef typename SplitterTraits<term_t>::iterator_t term_iterator_t;
+
         Splitter(const std::string& shard,
                  const std::string& field,
                  const std::string& output_dir,
                  size_t num_splits);
 
+        Splitter(const std::string& shardname,
+                 const std::string& field,
+                 term_iterator_t    term_iterator,
+                 const std::string& output_dir,
+                 size_t num_splits);
+
+        const std::string& shardname() const { return _shardname; }
         const std::vector<std::string>& splits() const { return _splits; }
         void run();
 
     private:
+        std::string name_of(const std::string& shard) const;
+
         FILE* open_split(const std::string& split);
 
         void encode(std::ostream& os, const term_t& term);
 
-        const std::string        _shard;
-        const std::string        _field;
+        const std::string _shardname;
+        const std::string _field;
+        term_iterator_t   _term_iterator;
+
         std::vector<std::string> _splits;
     };
-
-
-    template <typename term_t> struct SplitterTraits { typedef void                 iterator_t; };
-    template <> struct SplitterTraits<IntTerm>       { typedef int_term_iterator    iterator_t; };
-    template <> struct SplitterTraits<StringTerm>    { typedef string_term_iterator iterator_t; };
 
     template <typename term_t>
     Splitter<term_t>::Splitter(const std::string& shard,
                                const std::string& field,
                                const std::string& output_dir,
                                size_t num_splits)
-        : _shard(shard)
+        : Splitter(name_of(shard), field,
+                   term_iterator_t(Shard::term_filename<term_t>(shard, field)),
+                   output_dir, num_splits)
+    { }
+
+    template <typename term_t>
+    Splitter<term_t>::Splitter(const std::string& shardname,
+                               const std::string& field,
+                               term_iterator_t    term_iterator,
+                               const std::string& output_dir,
+                               size_t             num_splits)
+        : _shardname(shardname)
         , _field(field)
+        , _term_iterator(term_iterator)
     {
-        const std::string::size_type pos(shard.find_last_of('/'));
-        const std::string file_name(pos == std::string::npos ?
-                                    shard : shard.substr(pos + 1));
         for (size_t split(0); split < num_splits; ++split) {
             std::ostringstream os;
-            os << output_dir << '/' << file_name << '.' << split;
+            os << output_dir << '/' << _shardname << '.' << split;
             _splits.push_back(os.str());
         }
     }
@@ -69,9 +87,9 @@ namespace imhotep {
         for (std::string split: splits()) {
             split_files.push_back(new std::ofstream(split.c_str()));
         }
-        std::hash<typename term_t::id_t>            hash_fun;
-        typename SplitterTraits<term_t>::iterator_t it(Shard::term_filename<term_t>(_shard, _field));
-        typename SplitterTraits<term_t>::iterator_t end;
+        std::hash<typename term_t::id_t> hash_fun;
+        term_iterator_t it(_term_iterator);
+        term_iterator_t end;
         while (it != end) {
             const size_t   split(hash_fun(it->id()) % _splits.size());
             std::ofstream& of(*split_files[split]);
@@ -80,6 +98,14 @@ namespace imhotep {
             ++it;
         }
         for (auto os_ptr: split_files) delete os_ptr;
+    }
+
+    template <typename term_t>
+    std::string Splitter<term_t>::name_of(const std::string& shard) const
+    {
+        const std::string::size_type pos(shard.find_last_of('/'));
+        return pos == std::string::npos ?
+            shard : shard.substr(pos + 1);
     }
 
     template <typename term_t>
