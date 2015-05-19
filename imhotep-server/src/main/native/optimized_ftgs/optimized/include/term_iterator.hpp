792f73ea8e41339b9fb88117a7c0ec0cffcbd3ca
John C. Finley <johnf@indeed.com>
2015-05-19 11:28:09 -0500

    Added docid_base to term_iterator.

--- imhotep-server/src/main/native/optimized_ftgs/optimized/include/term_iterator.hpp
+++ imhotep-server/src/main/native/optimized_ftgs/optimized/include/term_iterator.hpp
@@ -4,6 +4,7 @@
 #include <cassert>
 #include <cstring>
 #include <memory>
+#include <cstdint>
 #include <string>
 
 #include <boost/iterator/iterator_facade.hpp>
@@ -25,13 +26,17 @@ namespace imhotep {
     public:
         term_iterator() { }
 
-        term_iterator(const std::string& filename)
-            : _view(std::make_shared<MMappedVarIntView>(filename)) {
+        term_iterator(const std::string& filename,
+                      int64_t docid_base=0)
+            : _view(std::make_shared<MMappedVarIntView>(filename))
+            , _docid_base(docid_base) {
             increment();
         }
 
-        term_iterator(const char* begin, const char* end)
-            : _view(std::make_shared<VarIntView>(begin, end)) {
+        term_iterator(const char* begin, const char* end,
+                      int64_t docid_base=0)
+            : _view(std::make_shared<VarIntView>(begin, end))
+            , _docid_base(docid_base) {
             increment();
         }
 
@@ -45,6 +50,8 @@ namespace imhotep {
         const term_t& dereference() const { return _current; }
 
         std::shared_ptr<VarIntView> _view = std::make_shared<VarIntView>();
+
+        int64_t _docid_base = 0;
 
         term_t _current;
 
@@ -63,11 +70,11 @@ namespace imhotep {
     template<>
     void term_iterator<IntTerm>::increment() {
         if (!_view->empty()) {
-            const long id_delta(_view->read_varint<long>(_view->read()));
-            const long offset_delta(_view->read_varint<long>(_view->read()));
-            const long doc_freq(_view->read_varint<long>(_view->read()));
+            const int64_t id_delta(_view->read_varint<int64_t>(_view->read()));
+            const int64_t offset_delta(_view->read_varint<int64_t>(_view->read()));
+            const int64_t doc_freq(_view->read_varint<int64_t>(_view->read()));
             _current = IntTerm(_current.id() + id_delta,
-                               _current.doc_offset() + offset_delta,
+                               _docid_base + _current.doc_offset() + offset_delta,
                                doc_freq);
         }
         else {
@@ -78,17 +85,17 @@ namespace imhotep {
     template<>
     void term_iterator<StringTerm>::increment() {
         if (!_view->empty()) {
-            const long erase(_view->read_varint<long>(_view->read()));
-            const long append(_view->read_varint<long>(_view->read()));
+            const int64_t erase(_view->read_varint<int64_t>(_view->read()));
+            const int64_t append(_view->read_varint<int64_t>(_view->read()));
             _id_buffer.erase(_id_buffer.size() - erase, erase);
             for (size_t count(0); count < size_t(append); ++count) {
                 _id_buffer.push_back(_view->read());
             }
 
-            const long offset_delta(_view->read_varint<long>(_view->read()));
-            const long doc_freq(_view->read_varint<long>(_view->read()));
+            const int64_t offset_delta(_view->read_varint<int64_t>(_view->read()));
+            const int64_t doc_freq(_view->read_varint<int64_t>(_view->read()));
             _current = StringTerm(_id_buffer,
-                                  _current.doc_offset() + offset_delta,
+                                  _docid_base + _current.doc_offset() + offset_delta,
                                   doc_freq);
         }
         else {
