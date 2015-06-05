8aeba860af2bf00bce688896270d4e52f2dbe4be
ivana <ivana@indeed.com>
2015-06-05 14:55:38 -0500

    Adds tests to test_ftgs_runner.cpp

--- imhotep-server/src/main/native/optimized_ftgs/optimized/test/src/test_ftgs_runner.cpp
+++ imhotep-server/src/main/native/optimized_ftgs/optimized/test/src/test_ftgs_runner.cpp
@@ -13,6 +13,8 @@
 #include "ftgs_runner.hpp"
 #include "shard.hpp"
 #include "term_iterator.hpp"
+#include "entry_point.hpp"
+
 
 #define restrict __restrict__
 extern "C" {
@@ -23,117 +25,166 @@ extern "C" {
 using namespace std;
 using namespace imhotep;
 
-class ShardMetadata {
-    const Shard&     _shard;
-    const YAML::Node _metadata;
-public:
-    ShardMetadata(const Shard& shard)
-        : _shard(shard)
-        , _metadata(YAML::LoadFile(shard.dir() + "/metadata.txt"))
-    { }
+namespace test_ftgs_namespace{
+    class ShardMetadata {
+        const Shard&     _shard;
+        const YAML::Node _metadata;
+    public:
+        ShardMetadata(const Shard& shard)
+            : _shard(shard)
+            , _metadata(YAML::LoadFile(shard.dir() + "/metadata.txt"))
+        {cout << shard.dir() + "/metadata.txt" << endl; }
 
-    const Shard& shard() const { return _shard; }
+        const Shard& shard() const { return _shard; }
 
-    size_t num_docs() const { return _metadata["numDocs"].as<size_t>(); }
+        size_t num_docs() const { return _metadata["numDocs"].as<size_t>(); }
 
-    template <typename term_t>
-    pair<int64_t, int64_t> min_max(const std::string& field) const;
-};
+        template <typename term_t>
+        pair<int64_t, int64_t> min_max(const std::string& field) const;
+    };
 
-template<>
-pair<int64_t, int64_t> ShardMetadata::min_max<IntTerm>(const std::string& field) const
-{
-    pair<int64_t, int64_t> result(numeric_limits<int64_t>::max(), numeric_limits<int64_t>::min());
-    IntTermIterator it(_shard, field);
-    IntTermIterator end;
-    while (it != end) {
-        result.first  = min(it->id(), result.first);
-        result.second = max(it->id(), result.second);
-        ++it;
+    template<>
+    pair<int64_t, int64_t> ShardMetadata::min_max<IntTerm>(const std::string& field) const
+    {
+        pair<int64_t, int64_t> result(numeric_limits<int64_t>::max(), numeric_limits<int64_t>::min());
+        IntTermIterator it(_shard, field);
+        IntTermIterator end;
+        while (it != end) {
+            result.first  = min(it->id(), result.first);
+            result.second = max(it->id(), result.second);
+            ++it;
+        }
+        return result;
     }
-    return result;
-}
 
-template<>
-pair<int64_t, int64_t> ShardMetadata::min_max<StringTerm>(const std::string& field) const
-{
-    return make_pair(0, 0);
-}
-
-
-struct TableMetadata {
-    int32_t* sizes           = 0;
-    int32_t* vec_nums        = 0;
-    int32_t* offsets_in_vecs = 0;
-
-    TableMetadata(int num_cols,
-                  const int64_t * restrict mins,
-                  const int64_t * restrict maxes)
-        : sizes(get_sizes(num_cols, mins, maxes))
-        , vec_nums(get_vec_nums(num_cols, mins, maxes, sizes))
-        , offsets_in_vecs(get_offsets_in_vecs(num_cols, mins, maxes, sizes))
-    { }
-
-    TableMetadata(const TableMetadata& rhs) = delete;
-
-    ~TableMetadata() {
-        free(sizes);
-        free(vec_nums);
-        free(offsets_in_vecs);
+    template<>
+    pair<int64_t, int64_t> ShardMetadata::min_max<StringTerm>(const std::string& field) const
+    {
+        return make_pair(0, 0);
     }
-};
 
-Shard make_shard(const ShardMetadata&  metadata,
-                 const vector<string>& int_fields,
-                 const vector<string>& str_fields)
-{
-    vector<int64_t> mins;
-    vector<int64_t> maxes;
-    for (const string& field: int_fields) {
+
+    struct TableMetadata {
+        int32_t* sizes           = 0;
+        int32_t* vec_nums        = 0;
+        int32_t* offsets_in_vecs = 0;
+
+        TableMetadata(int num_cols,
+                      const int64_t * restrict mins,
+                      const int64_t * restrict maxes)
+            : sizes(get_sizes(num_cols, mins, maxes))
+            , vec_nums(get_vec_nums(num_cols, mins, maxes, sizes))
+            , offsets_in_vecs(get_offsets_in_vecs(num_cols, mins, maxes, sizes))
+        { }
+
+        TableMetadata(const TableMetadata& rhs) = delete;
+
+        ~TableMetadata() {
+            free(sizes);
+            free(vec_nums);
+            free(offsets_in_vecs);
+        }
+    };
+
+    Shard make_shard(const ShardMetadata&  metadata,
+                     const vector<string>& int_fields,
+                     const vector<string>& str_fields)
+    {
+        vector<int64_t> mins;
+        vector<int64_t> maxes;
+        for (const string& field: int_fields) {
             std::pair<int64_t, int64_t> min_max(metadata.min_max<IntTerm>(field));
             mins.push_back(min_max.first);
             maxes.push_back(min_max.second);
-    }
-    for (const string& field: str_fields) {
-        throw runtime_error("nyi: support for string fields");
-    }
+        }
+        //TODO FIX?
+        for (const string& field: str_fields) {
+            throw runtime_error("nyi: support for string fields");
+        }
 
-    const size_t num_cols(int_fields.size() + str_fields.size());
+        const size_t num_cols(int_fields.size() + str_fields.size());
 
-    TableMetadata table_meta(num_cols, mins.data(), maxes.data());
+        TableMetadata table_meta(num_cols, mins.data(), maxes.data());
 
-    Shard::packed_table_ptr table;
-    table.reset(packed_table_create(metadata.num_docs(),
+        Shard::packed_table_ptr table;
+
+        table = packed_table_create(metadata.num_docs(),
                                     mins.data(), maxes.data(),
                                     table_meta.sizes, table_meta.vec_nums, table_meta.offsets_in_vecs,
-                                    vector<int8_t>(num_cols, 0).data(), num_cols));
+                                    vector<int8_t>(num_cols, 0).data(), num_cols);
+        return Shard(metadata.shard().dir(), table);
+    }
 
-    return Shard(metadata.shard().dir(), table);
+
+
+    vector<Shard> create_shards_with_tables(const vector<Shard>&  shards,
+                                            const vector<string>& int_fields,
+                                            const vector<string>& str_fields){
+        vector<ShardMetadata> metadata(shards.begin(), shards.end());
+        vector<Shard> shards_with_tables;
+        transform(metadata.begin(), metadata.end(), back_inserter(shards_with_tables),
+                  [&int_fields, &str_fields](const ShardMetadata& sm) {
+            return make_shard(sm, int_fields, str_fields);
+        });
+        return shards_with_tables;
+    }
+
+
+
+    void test_ftgs_runner_short(const vector<Shard>&  shards,
+                                const vector<string>& int_fields,
+                                const vector<string>& str_fields,
+                                const string&         splits_dir,
+                                size_t                num_splits =7
+                                ){
+        vector<Shard> shards_with_tables=create_shards_with_tables(shards, int_fields, str_fields);
+        ExecutorService       executor;
+        const size_t num_workers= executor.num_workers();
+
+        FTGSRunner runner(shards_with_tables,
+                          int_fields,
+                          str_fields,
+                          splits_dir,
+                          num_splits,
+                          num_workers,
+                          executor);
+
+    }
+
+
+        void test_ftgs_runner_full(const vector<Shard>&  shards,
+                                   const vector<string>& int_fields,
+                                   const vector<string>& str_fields,
+                                   const string&         splits_dir,
+                                   size_t                num_splits,
+                                   const int             num_groups,
+                                   const int             num_metrics,
+                                   std::vector<int> &    socket_fds,
+                                   const bool            only_binary_metrics=false){
+        vector<Shard> shards_with_tables=create_shards_with_tables(shards, int_fields, str_fields);
+        ExecutorService       executor;
+        const size_t num_workers= executor.num_workers();
+
+        FTGSRunner runner(shards_with_tables,
+                          int_fields,
+                          str_fields,
+                          splits_dir,
+                          num_splits,
+                          num_workers,
+                          executor);
+
+        imhotep::run(runner,
+            num_groups,
+            num_metrics,
+            only_binary_metrics,
+            shards_with_tables[0].table(),
+            socket_fds.data(),
+            executor); //from run_native_ftgs.hpp
+    }
 }
 
-void test_ftgs_runner(const vector<Shard>&  shards,
-                      const vector<string>& int_fields,
-                      const vector<string>& str_fields,
-                      const string&         split_dir,
-                      size_t                num_splits = 7)
-{
-    ExecutorService       executor;
-    vector<ShardMetadata> metadata(shards.begin(), shards.end());
-    vector<Shard> shards_with_tables;
-    transform(metadata.begin(), metadata.end(), back_inserter(shards_with_tables),
-              [&int_fields, &str_fields](const ShardMetadata& sm) {
-                  return make_shard(sm, int_fields, str_fields);
-              });
-
-    FTGSRunner runner(shards_with_tables,
-                      int_fields, str_fields, split_dir,
-                      num_splits,
-                      executor.num_workers(), // !@# fix wart in FTGSRunner ctor
-                      executor);
-
-    //    runner();
-}
-
+//TODO calculate int_fields,stringFields, numGroups, numStats, num_metrics and sockets based on the min info needed.
+//base yourself on the java code
 int main(int argc, char *argv[])
 {
     namespace fs = boost::filesystem;
@@ -141,7 +192,8 @@ int main(int argc, char *argv[])
 
     const vector<string> empty;
 
-    po::options_description desc("Allowed options");
+    po::options_description desc("At the moment there is no support for string fields.\n\
+                                 Allowed options");
     desc.add_options()
         ("help", "simple test for ftgs runner")
         ("shard-dir",  po::value<string>(),
@@ -154,13 +206,22 @@ int main(int argc, char *argv[])
          "directory to stash temp split files")
         ("num-splits", po::value<size_t>()->default_value(13, "UINT"),
          "number of splits to use")
+        ("full-test", po::value<bool>()->default_value(false, "BOOL"),
+              "needs num-groups and num-metrics set")
+
         ;
 
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
     po::notify(vm);
 
-    if (vm.count("help")) {
+    bool full_test = vm.count("full-test")&&
+                     vm["full-test"].as<bool>();
+
+    if (vm.count("help")||(
+            !vm.count("shard-dir") ||
+            !vm.count("int-fields")))
+    {
         cout << desc << endl;
         return 0;
     }
@@ -179,9 +240,28 @@ int main(int argc, char *argv[])
 
     const string split_dir(vm["split-dir"].as<string>());
     const size_t num_splits(vm["num-splits"].as<size_t>());
-
-    test_ftgs_runner(shards, int_fields, str_fields, split_dir, num_splits);
-
+    const size_t num_groups =1;
+    const size_t num_metrics=int_fields.size();
+    if (full_test){
+        std::vector<int> sockets(1,-1);
+        bool only_binary_metrics=false;
+        test_ftgs_namespace::test_ftgs_runner_full(shards,
+                                                    int_fields,
+                                                    str_fields,
+                                                    split_dir,
+                                                    num_splits,
+                                                    num_groups,
+                                                    num_metrics,
+                                                    sockets,
+                                                    only_binary_metrics);
+    }
+    else{
+        test_ftgs_namespace::test_ftgs_runner_short(shards,
+                                               int_fields,
+                                               str_fields,
+                                               split_dir,
+                                               num_splits);
+    }
     exit(0);
 }
 
