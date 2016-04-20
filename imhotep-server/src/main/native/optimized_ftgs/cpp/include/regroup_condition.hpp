305ff016a78e802543fbce0cca18824be4bb1f02
John C. Finley <johnf@indeed.com>
2016-04-20 12:42:20 -0500

    Added commentary to the native code regroup implementation.

--- imhotep-server/src/main/native/optimized_ftgs/cpp/include/regroup_condition.hpp
+++ imhotep-server/src/main/native/optimized_ftgs/cpp/include/regroup_condition.hpp
@@ -1,3 +1,18 @@
+/** @file regroup_condition.hpp - GroupMultiRemapRule condition representations
+
+    Regroup conditions come in two basic flavors, equality and inequality,
+    specific to either integer or string terms; so four altogether. There are
+    also type-specific null conditions which brings the total count to six.
+
+    All conditions share some basic properties, and so both equality and
+    inequality varieties, so they fit into a natural class hierarchy. That said,
+    we need to store them compactly and eschew the overhead of virtual dispatch
+    (measured to around 20% in my test case). So instead of using vanilla C++
+    inheritance I went with the boost::variant package, which essentially
+    provides syntactic sugar over discriminated unions.
+
+    All of which is to say, there is method to some of this madness.
+ */
 #ifndef REGROUP_CONDITION
 #define REGROUP_CONDITION
 
@@ -15,10 +30,18 @@
 
 namespace imhotep {
 
+    /** @defgroup ComparisonTags @{ */
     class equality_tag   { };
     class inequality_tag { };
     class null_tag       { };
+    /** @} */
 
+
+    /** Common Condition state.
+
+        term_t should be one of int64_t or std::string
+        comparison_t should be a ComparisonTag
+     */
     template <typename term_t, typename comparison_t>
     class Condition {
     public:
@@ -41,8 +64,12 @@ namespace imhotep {
         term_t      _term;
     };
 
-    /**********************************************************************/
 
+    /** @defgroup EqualityConditions @{ */
+
+    /** EqualityConditions track iterators into a sorted doc stream for a given
+        term.
+     */
     template <typename term_t>
     class EqualityCondition : public Condition<term_t, equality_tag> {
     public:
@@ -68,8 +95,15 @@ namespace imhotep {
     typedef EqualityCondition<int64_t>     IntEquality;
     typedef EqualityCondition<std::string> StrEquality;
 
-    /**********************************************************************/
+    /** @} EqualityConditions */
 
+
+    /** @defgroup InequalityConditions @{ */
+
+    /** InequalityConditions maintain a set of docs, represented as a bitset,
+        populated by all docs referencing all terms less than the condition's
+        term.
+    */
     template <typename term_t>
     class InequalityCondition : public Condition<term_t, inequality_tag> {
     public:
@@ -91,7 +125,10 @@ namespace imhotep {
     typedef InequalityCondition<int64_t>     IntInequality;
     typedef InequalityCondition<std::string> StrInequality;
 
-    /**********************************************************************/
+    /** @} InequalityConditions */
+
+
+    /** @defgroup NullConditions @{ */
 
     template <typename term_t>
     class NullCondition : public Condition<term_t, null_tag> {
@@ -109,7 +146,10 @@ namespace imhotep {
     typedef NullCondition<int64_t>     IntNull;
     typedef NullCondition<std::string> StrNull;
 
-    /**********************************************************************/
+    /** @} NullConditions */
+
+
+    /** @defgroup RegroupCondition definition and operations. @{ */
 
     typedef boost::variant<IntEquality, IntInequality,
                            StrEquality, StrInequality,
@@ -146,6 +186,8 @@ namespace imhotep {
     private:
         Shard& _shard;
     };
+
+    /** @} RegroupCondition */
 
 } // namespace imhotep
 
