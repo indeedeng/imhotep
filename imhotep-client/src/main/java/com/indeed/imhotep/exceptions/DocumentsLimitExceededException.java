409b57aa4c673b5eef266902087ad3230de1a5dd
Vladimir Markin <vladimir@indeed.com>
2016-08-02 03:31:28 -0500

    Added ImhotepErrorResolver which can be used by IQL clients to resolve Imhotep internal exceptions to a more user friendly error message

---
+++ imhotep-client/src/main/java/com/indeed/imhotep/exceptions/DocumentsLimitExceededException.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2014 Indeed Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.indeed.imhotep.exceptions;
+
+/**
+ * @author vladimir
+ */
+
+public class DocumentsLimitExceededException extends LimitExceededException {
+    /**
+     * Constructs an <code>LimitExceededException</code> with no
+     * detail message.
+     */
+    public DocumentsLimitExceededException() {
+        super();
+    }
+
+    /**
+     * Constructs an <code>LimitExceededException</code> with the
+     * specified detail message.
+     *
+     * @param   s   the detail message.
+     */
+    public DocumentsLimitExceededException(String s) {
+        super(s);
+    }
+
+    /**
+     * Constructs a new exception with the specified detail message and
+     * cause.
+     *
+     * <p>Note that the detail message associated with <code>cause</code> is
+     * <i>not</i> automatically incorporated in this exception's detail
+     * message.
+     *
+     * @param  message the detail message (which is saved for later retrieval
+     *         by the {@link Throwable#getMessage()} method).
+     * @param  cause the cause (which is saved for later retrieval by the
+     *         {@link Throwable#getCause()} method).  (A <tt>null</tt> value
+     *         is permitted, and indicates that the cause is nonexistent or
+     *         unknown.)
+     */
+    public DocumentsLimitExceededException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
