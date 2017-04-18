cd373e6fb85c78612b1f025f99c296e537ff49bd
Adil Ibragimov <aibragimov@indeed.com>
2017-04-18 16:21:49 +0900

    NOBUG: change TestShardStore for better logging.

--- imhotep-server/src/test/java/com/indeed/imhotep/service/TestShardStore.java
+++ imhotep-server/src/test/java/com/indeed/imhotep/service/TestShardStore.java
@@ -13,28 +13,26 @@
  */
 package com.indeed.imhotep.service;
 
+import com.google.common.base.Throwables;
 import com.indeed.imhotep.ShardInfo;
-import com.indeed.imhotep.io.TestFileUtils;
 import com.indeed.lsmtree.core.Store;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import org.apache.commons.lang.RandomStringUtils;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.SortedMap;
 import java.util.TreeMap;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 public class TestShardStore {
 
@@ -43,29 +41,29 @@ public class TestShardStore {
     @Rule
     public final TemporaryFolder rootTestDir = new TemporaryFolder();
 
-    private Path storeDir   = null;
+    private Path storeDir = null;
 
     @Before
     public void setUp() throws Exception {
-        storeDir   = rootTestDir.newFolder("store").toPath();
+        storeDir = rootTestDir.newFolder("store").toPath();
     }
 
     @Test public void testShardStore() {
         try {
-            RandomEntries before = new RandomEntries(64, 512);
+            final SortedMap<ShardStore.Key, ShardStore.Value> before = generateRandomEntries(64, 512);
 
             /* Create the store and verify that it has everything in it. */
             try (ShardStore store = new ShardStore(storeDir)) {
-               for (Map.Entry<ShardStore.Key, ShardStore.Value> entry: before.entrySet()) {
+               for (final Map.Entry<ShardStore.Key, ShardStore.Value> entry: before.entrySet()) {
                    store.put(entry.getKey(), entry.getValue());
                }
 
-               TreeMap<ShardStore.Key, ShardStore.Value> after =
+               final Map<ShardStore.Key, ShardStore.Value> after =
                    new TreeMap<ShardStore.Key, ShardStore.Value>();
-               Iterator<Store.Entry<ShardStore.Key, ShardStore.Value>> it =
+               final Iterator<Store.Entry<ShardStore.Key, ShardStore.Value>> it =
                    store.iterator();
                while (it.hasNext()) {
-                   Store.Entry<ShardStore.Key, ShardStore.Value> entry = it.next();
+                   final Store.Entry<ShardStore.Key, ShardStore.Value> entry = it.next();
                    after.put(entry.getKey(), entry.getValue());
                }
                assertEquals(before, after);
@@ -78,28 +76,28 @@ public class TestShardStore {
 
             /* Reopen the store and verify that it has everything in it. */
             try (ShardStore store = new ShardStore(storeDir)) {
-               TreeMap<ShardStore.Key, ShardStore.Value> after =
+               final Map<ShardStore.Key, ShardStore.Value> after =
                    new TreeMap<ShardStore.Key, ShardStore.Value>();
-               Iterator<Store.Entry<ShardStore.Key, ShardStore.Value>> it =
+               final Iterator<Store.Entry<ShardStore.Key, ShardStore.Value>> it =
                    store.iterator();
                while (it.hasNext()) {
-                   Store.Entry<ShardStore.Key, ShardStore.Value> entry = it.next();
+                   final Store.Entry<ShardStore.Key, ShardStore.Value> entry = it.next();
                    after.put(entry.getKey(), entry.getValue());
                }
                assertEquals(before, after);
             }
         }
-        catch (Exception ex) {
-            fail(ex.toString());
+        catch (final Exception ex) {
+            Throwables.propagate(ex);
         }
     }
 
     @Test public void testShardInfoList() {
         try {
-            RandomEntries entries = new RandomEntries(64, 512);
+            final SortedMap<ShardStore.Key, ShardStore.Value> entries = generateRandomEntries(64, 512);
 
-            List<ShardInfo> expected = new ObjectArrayList<ShardInfo>(entries.size());
-            for (Map.Entry<ShardStore.Key, ShardStore.Value> entry: entries.entrySet()) {
+            final List<ShardInfo> expected = new ObjectArrayList<ShardInfo>(entries.size());
+            for (final Map.Entry<ShardStore.Key, ShardStore.Value> entry: entries.entrySet()) {
                 final ShardStore.Key   key   = entry.getKey();
                 final ShardStore.Value value = entry.getValue();
                 final ShardInfo shardInfo =
@@ -112,7 +110,7 @@ public class TestShardStore {
             Collections.sort(expected, ShardInfoList.comparator);
 
             try (ShardStore store = new ShardStore(storeDir)) {
-               for (Map.Entry<ShardStore.Key, ShardStore.Value> entry: entries.entrySet()) {
+               for (final Map.Entry<ShardStore.Key, ShardStore.Value> entry: entries.entrySet()) {
                    store.put(entry.getKey(), entry.getValue());
                }
             }
@@ -124,21 +122,21 @@ public class TestShardStore {
 
             /* Reopen the store and verify that it has everything in it. */
             try (ShardStore store = new ShardStore(storeDir)) {
-               ShardInfoList actual = new ShardInfoList(store);
+               final ShardInfoList actual = new ShardInfoList(store);
                assertEquals(expected, actual);
          }
         }
-        catch (Exception ex) {
-            fail(ex.toString());
+        catch (final Exception ex) {
+            Throwables.propagate(ex);
         }
     }
 
     @Test public void testDatasetInfoList() {
         try {
-            RandomEntries entries = new RandomEntries(64, 512);
+            final SortedMap<ShardStore.Key, ShardStore.Value> entries = generateRandomEntries(64, 512);
 
             try (ShardStore store = new ShardStore(storeDir)) {
-               for (Map.Entry<ShardStore.Key, ShardStore.Value> entry: entries.entrySet()) {
+               for (final Map.Entry<ShardStore.Key, ShardStore.Value> entry: entries.entrySet()) {
                    store.put(entry.getKey(), entry.getValue());
                }
             }
@@ -150,13 +148,13 @@ public class TestShardStore {
 
             /* Reopen the store and verify that it has everything in it. */
             try (ShardStore store = new ShardStore(storeDir)) {
-                DatasetInfoList actual = new DatasetInfoList(store);
+                final DatasetInfoList actual = new DatasetInfoList(store);
                 /* TODO(johnf): test the content in some meaningful way (without
                  just recreating the DatasetInfoList logic in this test. */
          }
         }
-        catch (Exception ex) {
-            fail(ex.toString());
+        catch (final Exception ex) {
+            Throwables.propagate(ex);
         }
     }
 
@@ -170,25 +168,26 @@ public class TestShardStore {
         return result;
     }
 
-    private class RandomEntries extends TreeMap<ShardStore.Key, ShardStore.Value> {
-        RandomEntries(int numDatasets, int maxShardsPerDataset) {
-            for (int cDataset = 0; cDataset < numDatasets; ++cDataset) {
-                final String dataset = RandomStringUtils.randomAlphanumeric(16);
-                final int numShards = 1 + rng.nextInt(maxShardsPerDataset);
-                for (int cShard = 0; cShard < numShards; ++cShard) {
-                    final String shardId = RandomStringUtils.randomAlphanumeric(64);
-                    final int numDocs = 1 + rng.nextInt(256);
-                    final long version = 1 + rng.nextInt(3);
-                    final ObjectArrayList<String> intFields = newRandomFieldList();
-                    final ObjectArrayList<String> strFields = newRandomFieldList();
-                    final String shardDir =
-                        "/var/shards/" + shardId + Long.valueOf(version).toString();
-                    final ShardStore.Key key = new ShardStore.Key(dataset, shardId);
-                    final ShardStore.Value value =
-                        new ShardStore.Value(shardDir, numDocs, version, intFields, strFields);
-                    put(key, value);
-                }
+    private SortedMap<ShardStore.Key, ShardStore.Value> generateRandomEntries(final int numDatasets,
+                                                                              final int maxShardsPerDataset) {
+        final SortedMap<ShardStore.Key, ShardStore.Value> result = new TreeMap<>();
+        for (int cDataset = 0; cDataset < numDatasets; ++cDataset) {
+            final String dataset = RandomStringUtils.randomAlphanumeric(16);
+            final int numShards = 1 + rng.nextInt(maxShardsPerDataset);
+            for (int cShard = 0; cShard < numShards; ++cShard) {
+                final String shardId = RandomStringUtils.randomAlphanumeric(64);
+                final int numDocs = 1 + rng.nextInt(256);
+                final long version = 1 + rng.nextInt(3);
+                final ObjectArrayList<String> intFields = newRandomFieldList();
+                final ObjectArrayList<String> strFields = newRandomFieldList();
+                final String shardDir =
+                    "/var/shards/" + shardId + Long.valueOf(version).toString();
+                final ShardStore.Key key = new ShardStore.Key(dataset, shardId);
+                final ShardStore.Value value =
+                    new ShardStore.Value(shardDir, numDocs, version, intFields, strFields);
+                result.put(key, value);
             }
         }
+        return result;
     }
 }
