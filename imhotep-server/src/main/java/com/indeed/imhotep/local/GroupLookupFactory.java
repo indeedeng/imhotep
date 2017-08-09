797df4f2318a5b052e3c3c4cd95c3b74e5a8e7d0
Adil Ibragimov <aibragimov@indeed.com>
2017-08-09 13:49:56 +0900

    jira/IMTEPD-303: refactoring of GroupLookupFactory

--- imhotep-server/src/main/java/com/indeed/imhotep/local/GroupLookupFactory.java
+++ imhotep-server/src/main/java/com/indeed/imhotep/local/GroupLookupFactory.java
@@ -20,34 +20,107 @@ public class GroupLookupFactory {
     private GroupLookupFactory() {
     }
 
+    private interface GroupLookupCreator {
+        GroupLookup createLookup(ImhotepLocalSession session, int size);
+        long calcMemUsageForSize(int size);
+        int getMaxGroup();
+    }
+
+    private static final GroupLookupCreator[] CREATORS = new GroupLookupCreator[] {
+        //BitSetGroupLookup
+        new GroupLookupCreator() {
+            @Override
+            public GroupLookup createLookup(final ImhotepLocalSession session, final int size) {
+                return new BitSetGroupLookup(session, size);
+            }
+
+            @Override
+            public long calcMemUsageForSize(final int size) {
+                return BitSetGroupLookup.calcMemUsageForSize(size);
+            }
+
+            @Override
+            public int getMaxGroup() {
+                return 1;
+            }
+        },
+
+        //ByteGroupLookup
+        new GroupLookupCreator() {
+            @Override
+            public GroupLookup createLookup(final ImhotepLocalSession session, final int size) {
+                return new ByteGroupLookup(session, size);
+            }
+
+            @Override
+            public long calcMemUsageForSize(final int size) {
+                return ByteGroupLookup.calcMemUsageForSize(size);
+            }
+
+            @Override
+            public int getMaxGroup() {
+                return 255;
+            }
+        },
+
+        //CharGroupLookup
+        new GroupLookupCreator() {
+            @Override
+            public GroupLookup createLookup(final ImhotepLocalSession session, final int size) {
+                return new CharGroupLookup(session, size);
+            }
+
+            @Override
+            public long calcMemUsageForSize(final int size) {
+                return CharGroupLookup.calcMemUsageForSize(size);
+            }
+
+            @Override
+            public int getMaxGroup() {
+                return 65535;
+            }
+        },
+
+        //IntGroupLookup
+        new GroupLookupCreator() {
+            @Override
+            public GroupLookup createLookup(final ImhotepLocalSession session, final int size) {
+                return new IntGroupLookup(session, size);
+            }
+
+            @Override
+            public long calcMemUsageForSize(final int size) {
+                return IntGroupLookup.calcMemUsageForSize(size);
+            }
+
+            @Override
+            public int getMaxGroup() {
+                return Integer.MAX_VALUE;
+            }
+        }
+    };
+
+    private static GroupLookupCreator findCreator(final int groupCount) {
+        for(final GroupLookupCreator creator : CREATORS) {
+            if( groupCount <= creator.getMaxGroup()) {
+                return creator;
+            }
+        }
+
+        throw new RuntimeException("Unreachable code in GroupLookupCreator.findCreator");
+    }
+
     public static GroupLookup create(final int maxGroup,
                                      final int size,
                                      final ImhotepLocalSession session,
                                      final MemoryReservationContext memory) throws ImhotepOutOfMemoryException {
-        final GroupLookup newLookup;
-        if (maxGroup < 2) { // 8L * ((size + 64) >> 6)
-            if (!memory.claimMemory(BitSetGroupLookup.calcMemUsageForSize(size))) {
-                throw new ImhotepOutOfMemoryException();
-            }
-            newLookup = new BitSetGroupLookup(session, size);
-        } else if (maxGroup < 256) {
-            if (!memory.claimMemory(ByteGroupLookup.calcMemUsageForSize(size))) {
-                throw new ImhotepOutOfMemoryException();
-            }
-            newLookup = new ByteGroupLookup(session, size);
-        } else if (maxGroup < 65536) {
-            if (!memory.claimMemory(CharGroupLookup.calcMemUsageForSize(size))) {
-                throw new ImhotepOutOfMemoryException();
-            }
-            newLookup = new CharGroupLookup(session, size);
-        } else {
-            if (!memory.claimMemory(IntGroupLookup.calcMemUsageForSize(size))) {
-                throw new ImhotepOutOfMemoryException();
-            }
-            newLookup = new IntGroupLookup(session, size);
-        }
 
-        return newLookup;
+        final GroupLookupCreator lookupCreator = findCreator(maxGroup);
+        final long memoryUsage = lookupCreator.calcMemUsageForSize(size);
+        if (!memory.claimMemory(memoryUsage)) {
+            throw new ImhotepOutOfMemoryException();
+        }
+        return lookupCreator.createLookup(session, size);
     }
 
     public static GroupLookup resize(final GroupLookup existingGL,
@@ -59,18 +132,22 @@ public class GroupLookupFactory {
             /* need a bigger group */
             newGL = create(maxGroup, existingGL.size(), existingGL.getSession(), memory);
         } else {
-            /* maybe the group lookup can be shrunk */
+            /* check if the group lookup can be shrunk */
             final int newMaxgroup = Math.max(maxGroup, existingGL.getNumGroups());
-            if ((float) newMaxgroup > 0.7f * existingGL.maxGroup()) {
+            final GroupLookupCreator factory = findCreator(newMaxgroup);
+            final long newMemoryUsage = factory.calcMemUsageForSize(existingGL.size());
+            if(newMemoryUsage >= existingGL.memoryUsed()) {
+                /* can't decrease memory by using another lookup */
                 return existingGL;
             }
-    
+
             /* try to shrink the GroupLookup */
-            try {
-                newGL = create(newMaxgroup, existingGL.size(), existingGL.getSession(), memory);
-            } catch (final ImhotepOutOfMemoryException e) {
+            if(!memory.claimMemory(newMemoryUsage)) {
+                /* not enough memory to create new lookup. */
                 return existingGL;
             }
+
+            newGL = factory.createLookup(existingGL.getSession(), existingGL.size());
         }
         existingGL.copyInto(newGL);
         memory.releaseMemory(existingGL.memoryUsed());
