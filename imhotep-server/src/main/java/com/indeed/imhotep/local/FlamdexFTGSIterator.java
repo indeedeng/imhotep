23ca7b67016fe5d4e295e6ae330ea96ee6dd3a99
Vladimir Markin <vladimir@indeed.com>
2018-04-12 17:05:39 -0500

    Updated copyright header

--- imhotep-server/src/main/java/com/indeed/imhotep/local/FlamdexFTGSIterator.java
+++ imhotep-server/src/main/java/com/indeed/imhotep/local/FlamdexFTGSIterator.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2014 Indeed Inc.
-n *
+ * Copyright (C) 2018 Indeed Inc.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  *
--- imhotep-server/src/test/java/com/indeed/imhotep/io/FileSerializationBenchmark.java
+++ imhotep-server/src/test/java/com/indeed/imhotep/io/FileSerializationBenchmark.java
@@ -1,7 +1,7 @@
 /*
- * Copyright (C) 2014 Indeed Inc.
+ * Copyright (C) 2018 Indeed Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this path except
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
