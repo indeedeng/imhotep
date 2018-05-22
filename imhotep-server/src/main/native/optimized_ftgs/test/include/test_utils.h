44276f6157e4827dfdf6589391aaa59e22d90cfa
Adil Ibragimov <aibragimov@indeed.com>
2018-05-22 18:58:27 +0900

    jira/IMTEPD-371: Delete not used native code and recompile lib

--- imhotep-server/src/main/native/optimized_ftgs/test/include/test_utils.h
+++ imhotep-server/src/main/native/optimized_ftgs/test/include/test_utils.h
@@ -5,58 +5,12 @@
 #define restrict __restrict__
 #endif
 
-extern "C" {
-#include "test_patch.h"
-}
-
 #include <algorithm>
 #include <array>
 #include <cassert>
 #include <iostream>
 #include <iterator>
 #include <vector>
-
-typedef int     DocId;
-typedef int32_t GroupId;
-typedef int64_t Metric;
-
-
-template <size_t n_metrics>
-struct Metrics : public std::array<Metric, n_metrics>
-{
-  Metrics() { std::fill(this->begin(), this->end(), 0); }
-};
-
-template <size_t n_metrics>
-std::ostream& operator<<(std::ostream& os, const Metrics<n_metrics>& row) {
-  for (auto element: row) os << element << " ";
-  return os;
-}
-
-
-template <size_t n_metrics>
-struct ShardAttrs {
-    typedef Metrics<n_metrics> metrics_t;
-
-    int32_t* sizes           = 0;
-    int32_t* vec_nums        = 0;
-    int32_t* offsets_in_vecs = 0;
-
-    ShardAttrs(const metrics_t& mins, const metrics_t& maxes)
-        : sizes(get_sizes(n_metrics, mins.data(), maxes.data()))
-        , vec_nums(get_vec_nums(n_metrics, mins.data(), maxes.data(), sizes))
-        , offsets_in_vecs(get_offsets_in_vecs(n_metrics, mins.data(), maxes.data(), sizes))
-    { }
-
-    ~ShardAttrs() {
-        free(sizes);
-        free(vec_nums);
-        free(offsets_in_vecs);
-    }
-
-    ShardAttrs(const ShardAttrs& rhs) = delete;
-};
-
 
 template <typename int_t = int, typename iterator, typename buffer_t>
 void varint_encode(iterator begin, iterator end, buffer_t& out)
@@ -102,56 +56,6 @@ void varint_encode(iterator begin, iterator end, buffer_t& out)
     ++current;
   }
 }
-
-template <typename iterator, typename buffer_t>
-void doc_ids_encode(iterator begin, iterator end, buffer_t& out)
-{
-  std::vector<uint32_t> deltas;
-  uint32_t              value(0);
-
-  for (iterator current(begin); current != end; ++current) {
-    deltas.push_back(*current - value);
-    value = *current;
-  }
-
-  varint_encode(deltas.begin(), deltas.end(), out);
-}
-
-class VarIntView
-{
-protected:
-    const char* _begin  = 0;
-    const char* _end    = 0;
-
-public:
-    VarIntView(const char* begin=0, const char* end=0)
-        : _begin(begin) , _end(end)
-    { }
-
-    const char* begin() const { return _begin; }
-    const char* end()   const { return _end;   }
-
-    bool empty() const { return _begin >= _end; }
-
-    uint8_t read() {
-        assert(!empty());
-        const char result(empty() ? -1 : *_begin);
-        ++_begin;
-        return result;
-    }
-
-    template <typename int_t>
-    int_t read_varint(uint8_t b=0) {
-        int_t result(0);
-        int   shift(0);
-        do {
-            result |= ((b & 0x7FL) << shift);
-            if (b < 0x80) return result;
-            shift += 7;
-            b = read();
-        } while (true);
-    }
-};
 
 template <class T, size_t N>
 std::ostream& operator<<(std::ostream& os, const std::array<T, N>& items)
