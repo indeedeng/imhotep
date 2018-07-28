d231e8ca72b9b5c2a80186d5dffbc4425f25de67
Jack Humphrey <jack@indeed.com>
2018-07-28 14:08:16 -0500

    bring install script over from master branch

---
+++ install-imhotep-maven-local.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+AWS="https://s3-us-west-2.amazonaws.com/indeedeng-imhotep-build"
+VERSION=`curl -s $AWS | egrep -o "imhotep-server-[0-9\.]+(-\w+)?.jar" | tail -1 | egrep -o "([0-9]+\.?)+(-\w+)"`
+
+function install_maven {
+	artifact=$1
+	type=$2
+
+	jarfile=$artifact-$VERSION.$type
+	pomfile=$artifact.pom
+	
+	wget $AWS/$jarfile
+	wget $AWS/$pomfile
+
+	mvn install:install-file -Dfile=$jarfile -DgroupId=com.indeed -DartifactId=$artifact -Dversion=$VERSION -Dpackaging=$type -DpomFile=$pomfile
+}
+
+install_maven imhotep-server jar
+install_maven imhotep-client jar
+install_maven imhotep-archive jar
+
+# install parent pom
+wget $AWS/imhotep.pom
+mvn install:install-file -Dfile=imhotep.pom -DgroupId=com.indeed -DartifactId=imhotep -Dversion=$VERSION -Dpackaging=xml -DpomFile=imhotep.pom
+
