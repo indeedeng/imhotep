0a8f22cabb4f2105c3703330dc8a1045ea6ee4c5
Jack Humphrey <jack@indeed.com>
2018-07-28 08:20:16 -0500

    fix protobuf path

--- prereqs/install-protobuf.sh
+++ prereqs/install-protobuf.sh
@@ -2,7 +2,7 @@
 set -e
 # check to see if protobuf folder is empty
 if [ ! -d "$HOME/protobuf/lib" ]; then
-  wget https://protobuf.googlecode.com/files/protobuf-2.5.0.tar.gz
+  wget https://github.com/google/protobuf/releases/download/v2.5.0/protobuf-2.5.0.tar.gz
   tar -xzvf protobuf-2.5.0.tar.gz
   cd protobuf-2.5.0 && ./configure --prefix=$HOME/protobuf && make && make install
 else
