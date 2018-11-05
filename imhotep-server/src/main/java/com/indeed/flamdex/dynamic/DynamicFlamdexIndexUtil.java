8801b8739cd611281e544702d332b87664408e36
Michihiko Sawa <michihiko@indeed.com>
2018-11-05 17:14:22 +0900

    III-36: Restruct to expose the logic simplifying dynamic index

--- imhotep-server/src/main/java/com/indeed/flamdex/dynamic/DynamicFlamdexIndexUtil.java
+++ imhotep-server/src/main/java/com/indeed/flamdex/dynamic/DynamicFlamdexIndexUtil.java
@@ -15,8 +15,14 @@
 package com.indeed.flamdex.dynamic;
 
 import com.google.common.base.Optional;
+import com.indeed.flamdex.api.DocIdStream;
+import com.indeed.flamdex.api.IntTermIterator;
+import com.indeed.flamdex.api.StringTermIterator;
 import com.indeed.flamdex.dynamic.locks.MultiThreadFileLockUtil;
 import com.indeed.flamdex.dynamic.locks.MultiThreadLock;
+import com.indeed.flamdex.simple.SimpleFlamdexWriter;
+import com.indeed.flamdex.writer.IntFieldWriter;
+import com.indeed.flamdex.writer.StringFieldWriter;
 import com.indeed.util.core.shell.PosixFileOperations;
 import com.indeed.util.io.Directories;
 
@@ -97,5 +103,117 @@ public final class DynamicFlamdexIndexUtil {
         } else {
             return false;
         }
+    }
+
+    public static class DocIdMapping {
+        private final int newNumDocs;
+        private final int[] newDocIds;
+        private final int[] offset;
+
+        private DocIdMapping(final int newNumDocs, @Nonnull final int[] newDocIds, @Nonnull final int[] offset) {
+            this.newNumDocs = newNumDocs;
+            this.newDocIds = newDocIds;
+            this.offset = offset;
+        }
+
+        /**
+         * @param segmentId      Index of the ald segment.
+         * @param docIdInSegment Segment-local document ID of the document.
+         * @return Segment-local ID of the document in the new segment, or -1 if it has been deleted.
+         */
+        int getNewDocId(final int segmentId, final int docIdInSegment) {
+            return newDocIds[docIdInSegment + offset[segmentId]];
+        }
+
+        int getNewNumDocs() {
+            return newNumDocs;
+        }
+    }
+
+    @Nonnull
+    static DocIdMapping mergeSegments(@Nonnull final Path outputPath, @Nonnull final List<SegmentReader> segmentReaders) throws IOException {
+        final int numSegments = segmentReaders.size();
+        Path dir = segmentReaders.get(0).getDirectory();
+        if (dir != null) {
+            dir = dir.getParent();
+        }
+        try (final DynamicFlamdexReader reader = new DynamicFlamdexReader(dir, segmentReaders)) {
+            final int numDocs = reader.getNumDocs();
+            final int[] newDocIds = new int[numDocs];
+
+            final int[] offset = new int[numSegments + 1];
+            for (int segmentIdx = 0; segmentIdx < segmentReaders.size(); segmentIdx++) {
+                offset[segmentIdx + 1] = offset[segmentIdx] + segmentReaders.get(segmentIdx).maxNumDocs();
+            }
+            int newNumDoc = 0;
+            for (int segmentId = 0; segmentId < segmentReaders.size(); ++segmentId) {
+                final SegmentReader segmentReader = segmentReaders.get(segmentId);
+                for (int docId = 0; docId < segmentReaders.get(segmentId).maxNumDocs(); ++docId) {
+                    if (segmentReader.isDeleted(docId)) {
+                        newDocIds[docId + offset[segmentId]] = -1;
+                    } else {
+                        newDocIds[docId + offset[segmentId]] = newNumDoc;
+                        newNumDoc++;
+                    }
+                }
+            }
+            try (final SimpleFlamdexWriter writer = new SimpleFlamdexWriter(outputPath, newNumDoc)) {
+                final int[] docIdBuf = new int[256];
+                try (final DocIdStream docIdStream = reader.getDocIdStream()) {
+                    for (final String field : reader.getIntFields()) {
+                        try (
+                                final IntFieldWriter intFieldWriter = writer.getIntFieldWriter(field);
+                                final IntTermIterator intTermIterator = reader.getIntTermIterator(field)
+                        ) {
+                            while (intTermIterator.next()) {
+                                intFieldWriter.nextTerm(intTermIterator.term());
+                                docIdStream.reset(intTermIterator);
+                                while (true) {
+                                    final int num = docIdStream.fillDocIdBuffer(docIdBuf);
+                                    for (int i = 0; i < num; ++i) {
+                                        final int docId = newDocIds[docIdBuf[i]];
+                                        if (docId >= 0) {
+                                            intFieldWriter.nextDoc(docId);
+                                        }
+                                    }
+                                    if (num < docIdBuf.length) {
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    for (final String field : reader.getStringFields()) {
+                        try (
+                                final StringFieldWriter stringFieldWriter = writer.getStringFieldWriter(field);
+                                final StringTermIterator stringTermIterator = reader.getStringTermIterator(field)
+                        ) {
+                            while (stringTermIterator.next()) {
+                                stringFieldWriter.nextTerm(stringTermIterator.term());
+                                docIdStream.reset(stringTermIterator);
+                                while (true) {
+                                    final int num = docIdStream.fillDocIdBuffer(docIdBuf);
+                                    for (int i = 0; i < num; ++i) {
+                                        final int docId = newDocIds[docIdBuf[i]];
+                                        if (docId >= 0) {
+                                            stringFieldWriter.nextDoc(docId);
+                                        }
+                                    }
+                                    if (num < docIdBuf.length) {
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                return new DocIdMapping(newNumDoc, newDocIds, offset);
+            }
+        }
+    }
+
+    @Nonnull
+    public static DocIdMapping mergeSegments(@Nonnull final Path outputPath, @Nonnull final DynamicFlamdexReader dynamicFlamdexReader) throws IOException {
+        return mergeSegments(outputPath, dynamicFlamdexReader.getSegmentReaders());
     }
 }
