d3b4c8182439e913b63eef757776f64f1122bed2
Michihiko Sawa <michihiko@indeed.com>
2019-01-29 14:39:57 +0900

    IMTEPD-474: Use RunAutomaton which makes automaton run significantly faster!

--- imhotep-server/src/main/java/com/indeed/imhotep/matcher/AutomatonStringTermMatcher.java
+++ imhotep-server/src/main/java/com/indeed/imhotep/matcher/AutomatonStringTermMatcher.java
@@ -3,14 +3,15 @@ package com.indeed.imhotep.matcher;
 import com.indeed.flamdex.api.StringTermIterator;
 import com.indeed.imhotep.automaton.Automaton;
 import com.indeed.imhotep.automaton.RegExp;
+import com.indeed.imhotep.automaton.RunAutomaton;
 
 import java.util.function.Consumer;
 
 class AutomatonStringTermMatcher implements StringTermMatcher {
-    private final Automaton automaton;
+    private final RunAutomaton automaton;
 
     AutomatonStringTermMatcher(final String regex) {
-        this.automaton = new RegExp(regex).toAutomaton();
+        this.automaton = new RunAutomaton(new RegExp(regex).toAutomaton());
     }
 
     @Override
