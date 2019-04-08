87ab81483e45c0472b04c174160512bd83b9f59f
Michihiko Sawa <michihiko@indeed.com>
2019-04-08 15:17:39 +0900

    IMTEPD-475: Remove redandunt catch

--- imhotep-client/src/main/java/com/indeed/imhotep/shardmasterrpc/MultiplexingRequestHandler.java
+++ imhotep-client/src/main/java/com/indeed/imhotep/shardmasterrpc/MultiplexingRequestHandler.java
@@ -15,7 +15,6 @@
 package com.indeed.imhotep.shardmasterrpc;
 
 import com.google.common.base.Stopwatch;
-import com.google.common.base.Throwables;
 import com.indeed.imhotep.protobuf.ShardMasterRequest;
 import com.indeed.imhotep.protobuf.ShardMasterResponse;
 import org.apache.log4j.Logger;
@@ -44,25 +43,19 @@ public class MultiplexingRequestHandler implements RequestHandler {
     public Iterable<ShardMasterResponse> handleRequest(final ShardMasterRequest request) {
         final Stopwatch stopwatch = Stopwatch.createStarted();
         try {
-            try {
-                final Iterable<ShardMasterResponse> result = requestHandler.handleRequest(request);
-                final long elapsed = stopwatch.elapsed(TimeUnit.MILLISECONDS);
-                statsEmitter.processed(METRIC_PROCESSED, request.getRequestType(), elapsed);
-                LOGGER.debug("Finished handling request " + request.getRequestType() + " from " + request.getNode().getHost() + " in " + elapsed + " ms");
-                return result;
-            } catch (final RuntimeException e) {
-                final Iterable<ShardMasterResponse> result = Collections.singletonList(ShardMasterResponse.newBuilder()
-                        .setResponseCode(ShardMasterResponse.ResponseCode.ERROR)
-                        .setErrorMessage(e.getMessage())
-                        .build());
-                statsEmitter.processed(METRIC_ERROR, request.getRequestType(), stopwatch.elapsed(TimeUnit.MILLISECONDS));
-                LOGGER.warn("Failed to handle a request from host " + request.getNode().getHost(), e);
-                return result;
-            }
-        } catch (final Throwable e) {
+            final Iterable<ShardMasterResponse> result = requestHandler.handleRequest(request);
             final long elapsed = stopwatch.elapsed(TimeUnit.MILLISECONDS);
-            statsEmitter.processed(METRIC_ERROR, request.getRequestType(), elapsed);
-            throw Throwables.propagate(e);
+            statsEmitter.processed(METRIC_PROCESSED, request.getRequestType(), elapsed);
+            LOGGER.debug("Finished handling request " + request.getRequestType() + " from " + request.getNode().getHost() + " in " + elapsed + " ms");
+            return result;
+        } catch (final Throwable e) {
+            final Iterable<ShardMasterResponse> result = Collections.singletonList(ShardMasterResponse.newBuilder()
+                    .setResponseCode(ShardMasterResponse.ResponseCode.ERROR)
+                    .setErrorMessage(e.getMessage())
+                    .build());
+            statsEmitter.processed(METRIC_ERROR, request.getRequestType(), stopwatch.elapsed(TimeUnit.MILLISECONDS));
+            LOGGER.warn("Failed to handle a request from host " + request.getNode().getHost(), e);
+            return result;
         }
     }
 }
