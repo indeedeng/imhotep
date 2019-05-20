ab464cee80593057b5667308902cd3d3cfff2193
Xueping Weng <xweng@indeed.com>
2019-05-20 14:13:04 +0900

    IMTEPD-498: Fix method call in tests

--- imhotep-server/src/test/java/com/indeed/imhotep/commands/TestCommandDeserializeRemoteImhotepRequest.java
+++ imhotep-server/src/test/java/com/indeed/imhotep/commands/TestCommandDeserializeRemoteImhotepRequest.java
@@ -88,7 +88,7 @@ public class TestCommandDeserializeRemoteImhotepRequest implements CommandsTest 
     @Before
     public void initialize() throws IOException {
         holder = sendDummyResponseReceiveCommand();
-        imhotepRemoteSession = new ImhotepRemoteSession(holder.host, holder.port, SESSION_ID, tempFileSizeHolder, false);
+        imhotepRemoteSession = new ImhotepRemoteSession(holder.host, holder.port, SESSION_ID, tempFileSizeHolder);
     }
 
     @Override @Test
--- imhotep-server/src/test/java/com/indeed/imhotep/service/TestLocalImhotepServiceCore.java
+++ imhotep-server/src/test/java/com/indeed/imhotep/service/TestLocalImhotepServiceCore.java
@@ -81,7 +81,7 @@ public class TestLocalImhotepServiceCore {
             final String sessionId = service.handleOpenSession("dataset", Collections.singletonList(
                     ShardBasicInfoMessage.newBuilder().setShardName("index20150601").build()),
                     "", "", "", (byte)0, 0, 0,
-                    false, "", null, 0);
+                    false, "", null, 0, false);
             service.handlePushStat(sessionId, "count()");
             final OutputStream os = new CloseableNullOutputStream();
             final Thread t = new Thread(new Runnable() {
