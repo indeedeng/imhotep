40a76aa83d0f77c3e7af328685b75c9551a6c3fe
Michihiko Sawa <michihiko@indeed.com>
2019-06-07 11:19:54 +0900

    IMTEPD-466: Take current stack trace as well

--- imhotep-client/src/main/java/com/indeed/imhotep/utils/StackTraceUtils.java
+++ imhotep-client/src/main/java/com/indeed/imhotep/utils/StackTraceUtils.java
@@ -21,9 +21,18 @@ public class StackTraceUtils {
         return new DummyThrowable(message, stackTraceElements);
     }
 
+    public static Throwable createThrowableWithStacktrace(final String message, final StackTraceElement[] stackTraceElements, @Nullable final Throwable cause) {
+        return new DummyThrowable(message, cause, stackTraceElements);
+    }
+
     private static class DummyThrowable extends Throwable {
         private DummyThrowable(final String message, final StackTraceElement[] stackTraceElements) {
             super(message, null, true, true);
+            this.setStackTrace(stackTraceElements);
+        }
+
+        private DummyThrowable(final String message, @Nullable final Throwable cause, final StackTraceElement[] stackTraceElements) {
+            super(message, cause, true, true);
             this.setStackTrace(stackTraceElements);
         }
     }
