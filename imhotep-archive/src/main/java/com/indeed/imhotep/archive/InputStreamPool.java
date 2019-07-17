1026f42e165972ca7645b3fa0d7dc3cc68f56fd1
David Wahler <dwahler@indeed.com>
2019-07-17 11:34:20 -0500

    IMTEPD-583: detect unsafe connection reuse in InputStreamPool

--- imhotep-archive/src/main/java/com/indeed/imhotep/archive/InputStreamPool.java
+++ imhotep-archive/src/main/java/com/indeed/imhotep/archive/InputStreamPool.java
@@ -1,13 +1,19 @@
 package com.indeed.imhotep.archive;
 
+import com.google.common.base.Preconditions;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.ReadOption;
+import org.apache.hadoop.io.ByteBufferPool;
 import org.apache.log4j.Logger;
 
 import java.io.Closeable;
+import java.io.FileDescriptor;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -58,6 +64,7 @@ public class InputStreamPool implements Closeable {
 
     private class StreamWrapper extends FSDataInputStream {
         private final Path path;
+        private boolean isClosed = false;
 
         StreamWrapper(final Path path, final FSDataInputStream in) {
             super(in);
@@ -66,6 +73,11 @@ public class InputStreamPool implements Closeable {
 
         @Override
         public void close() throws IOException {
+            if (isClosed) {
+                return;
+            }
+            isClosed = true;
+
             if (!streams.containsKey(path)) {
                 // return this stream to the pool
                 streams.put(path, (FSDataInputStream) in);
@@ -73,6 +85,130 @@ public class InputStreamPool implements Closeable {
                 // there's already another stream in the pool for this file, so close this one
                 super.close();
             }
+        }
+
+        private void ensureNotClosed() {
+            Preconditions.checkState(!isClosed, "attempt to reuse stream after it's been returned to pool");
+        }
+
+        @Override
+        public void seek(final long desired) throws IOException {
+            ensureNotClosed();
+            super.seek(desired);
+        }
+
+        @Override
+        public long getPos() throws IOException {
+            ensureNotClosed();
+            return super.getPos();
+        }
+
+        @Override
+        public int read(final long position, final byte[] buffer, final int offset, final int length) throws IOException {
+            ensureNotClosed();
+            return super.read(position, buffer, offset, length);
+        }
+
+        @Override
+        public void readFully(final long position, final byte[] buffer, final int offset, final int length) throws IOException {
+            ensureNotClosed();
+            super.readFully(position, buffer, offset, length);
+        }
+
+        @Override
+        public void readFully(final long position, final byte[] buffer) throws IOException {
+            ensureNotClosed();
+            super.readFully(position, buffer);
+        }
+
+        @Override
+        public boolean seekToNewSource(final long targetPos) throws IOException {
+            ensureNotClosed();
+            return super.seekToNewSource(targetPos);
+        }
+
+        @Override
+        public InputStream getWrappedStream() {
+            ensureNotClosed();
+            return super.getWrappedStream();
+        }
+
+        @Override
+        public int read(final ByteBuffer buf) throws IOException {
+            ensureNotClosed();
+            return super.read(buf);
+        }
+
+        @Override
+        public FileDescriptor getFileDescriptor() throws IOException {
+            ensureNotClosed();
+            return super.getFileDescriptor();
+        }
+
+        @Override
+        public void setReadahead(final Long readahead) throws IOException, UnsupportedOperationException {
+            ensureNotClosed();
+            super.setReadahead(readahead);
+        }
+
+        @Override
+        public void setDropBehind(final Boolean dropBehind) throws IOException, UnsupportedOperationException {
+            ensureNotClosed();
+            super.setDropBehind(dropBehind);
+        }
+
+        @Override
+        public ByteBuffer read(final ByteBufferPool bufferPool, final int maxLength, final EnumSet<ReadOption> opts) throws IOException, UnsupportedOperationException {
+            ensureNotClosed();
+            return super.read(bufferPool, maxLength, opts);
+        }
+
+        @Override
+        public void releaseBuffer(final ByteBuffer buffer) {
+            ensureNotClosed();
+            super.releaseBuffer(buffer);
+        }
+
+        @Override
+        public void unbuffer() {
+            ensureNotClosed();
+            super.unbuffer();
+        }
+
+        @Override
+        public int read() throws IOException {
+            ensureNotClosed();
+            return super.read();
+        }
+
+        @Override
+        public long skip(final long n) throws IOException {
+            ensureNotClosed();
+            return super.skip(n);
+        }
+
+        @Override
+        public int available() throws IOException {
+            ensureNotClosed();
+            return super.available();
+        }
+
+        @Override
+        public synchronized void mark(final int readlimit) {
+            ensureNotClosed();
+            super.mark(readlimit);
+        }
+
+        @Override
+        public synchronized void reset() throws IOException {
+            ensureNotClosed();
+            super.reset();
+        }
+
+        @Override
+        public boolean markSupported() {
+            ensureNotClosed();
+            return super.markSupported();
         }
     }
 }
